0ceeded74deed2cd22ba0b3ace855faa0ed179db
jkim734 <jkim734@gmail.com>
2021-08-12 15:00:23 +0900

    timer added

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text =
+            "선택시간: "+formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime(){
+        lblCurrentTime.text = String(count)
+        count = count + 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval:interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
 
